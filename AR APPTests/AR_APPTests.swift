45d33e687fd88a94bacc659911c9df89bc8c8932
sanuv9683 <sanuv9683@gmail.com>
2025-03-28 08:10:04 +0000

    Initial Commit

---
+++ AR APPTests/AR_APPTests.swift
@@ -0,0 +1,17 @@
+//
+//  AR_APPTests.swift
+//  AR APPTests
+//
+//  Created by Sanu Vithanage on 3/28/25.
+//
+
+import Testing
+@testable import AR_APP
+
+struct AR_APPTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
